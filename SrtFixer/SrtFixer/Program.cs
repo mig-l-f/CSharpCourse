f2bce361bf8dfd3c4ef3cb03894933c08463417b
Miguel Fernandes <miguelandre.fernandes@gmail.com>
2014-05-01 23:56:23 +0100

    Initial commit

---
+++ SrtFixer/SrtFixer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SrtFixer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Jagged array to allow use of foreach below
+            int[][] times = { new[] {3,16,45}, new[] {1,59,59}, new[] {9,38,0} };
+
+            foreach(int[] time in times) {
+                // Replace demo line below with a call to your method
+                Console.WriteLine("Hour is: {0}", time[0]);
+            }
+        }
+
+        // Write your time incrementing & printing method here
+		
+
+    }
+}
