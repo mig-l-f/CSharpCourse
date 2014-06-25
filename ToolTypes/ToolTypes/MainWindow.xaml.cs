b1224d270909250c2bd5901d875b9056d1cdc20b
mig-l-f <miguelandre.fernandes@gmail.com>
2014-06-26 00:22:49 +0100

    Cancel async task

--- ToolTypes/ToolTypes/MainWindow.xaml.cs
+++ ToolTypes/ToolTypes/MainWindow.xaml.cs
@@ -16,6 +16,8 @@ using System.Net;
 using System.Runtime.Serialization;
 using Newtonsoft.Json;
 using System.IO;
+using System.Threading;
+using System.Net.Http;
 
 namespace ToolTypes
 {
@@ -24,6 +26,9 @@ namespace ToolTypes
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        CancellationTokenSource cts;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -31,6 +36,11 @@ namespace ToolTypes
 
         private void time_Click(object sender, RoutedEventArgs e)
         {
+
+            if (cts != null)
+            {
+                cts.Cancel();
+            }
             // In this basic example, we pull over pre-formated text, and simply
             // assign it to the box.
             string url = "http://pmb.neongrit.net/aj/time.php";
@@ -41,6 +51,10 @@ namespace ToolTypes
 
         private void types_Click(object sender, RoutedEventArgs e)
         {
+            if (cts != null)
+            {
+                cts.Cancel();
+            }
             // In this section, the root of the JSON is an object, which
             // we then need to map to or store in an instance of a
             // class we have created.
@@ -53,6 +67,10 @@ namespace ToolTypes
 
         private void types_a_Click(object sender, RoutedEventArgs e)
         {
+            if (cts != null)
+            {
+                cts.Cancel();
+            }
             // However, in our scenerio, this is an unnecessary step, since
             // the logical root is actually the array of tool items. By this
             // I mean that there's only one property in the root JSON object,
@@ -69,6 +87,10 @@ namespace ToolTypes
 
         private void types_d_Click(object sender, RoutedEventArgs e)
         {
+            if (cts != null)
+            {
+                cts.Cancel();
+            }
             // What about when there's a delay? Perhaps the network is slow,
             // or the server busy.
             // This script, otherwise identical to the previous one,
@@ -84,27 +106,37 @@ namespace ToolTypes
 
         private async void types_sa_Click(object sender, RoutedEventArgs e)
         {
-            // Here we inform the system not to wait for this method to return
-            // with the async keyword.
-            // However, this isn't enough, since we must pause eventually to not
-            // break the code that will handle the eventual response.
+
+            if (cts != null) 
+            {
+                cts.Cancel();
+            }
+                
+            cts = new CancellationTokenSource();
+
             String url = "http://pmb.neongrit.net/aj/selectJSONarrayDelay.php?type=2";
-            WebRequest request = WebRequest.Create(url);
-            // Note here the await keyword, and the change of GetResponse() to
-            // GetResponseAsync().
-            WebResponse ws = await request.GetResponseAsync();
+            
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync(url, cts.Token);
 
-            List<Tool> tools = JsonConvert.DeserializeObject<List<Tool>>(new StreamReader(ws.GetResponseStream()).ReadToEnd());
-            tbOutput.Text = String.Format("ID {0} is a {1}", tools.First().toolID, tools.First().toolLabel);
-
-            // One issue though- making this asynchronous has allowed the
-            // user to continue interacting with the GUI, and indeed,
-            // pressing the buttons work as expected.
-            // But there is this looming spectre. If you press this button,
-            // then another, ego updating the label, in ten seconds' time,
-            // the label will be updated "automatically". Is that good UX?
-
-            // How could we fix it?
-        }
+                List<Tool> tools = JsonConvert.DeserializeObject<List<Tool>>(await response.Content.ReadAsStringAsync());
+                tbOutput.Text = String.Format("ID {0} is a {1}", tools.First().toolID, tools.First().toolLabel);
+            }
+            catch(OperationCanceledException)
+            {
+                Console.WriteLine("Download canceled.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Download failed.");
+            }
+            finally
+            {
+                cts = null;
+            }
+            
+        }        
     }
 }
