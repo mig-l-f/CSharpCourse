dae221f0f5cb915de704080177c15af30fe6070d
mig-l-f <miguelandre.fernandes@gmail.com>
2014-07-20 12:21:26 +0100

    UserManager View Model

---
+++ UserManager/UserManager/ViewModel/UserManagerViewModel.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using UserManager.Model.Users;
+using UserManager.Model.Encryption;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+//using System.Threading.Tasks;
+
+namespace UserManager.ViewModel
+{
+    public class UserManagerViewModel : IDataErrorInfo
+    {
+        #region Fields
+        private UserRepository repository_;
+        private User currentUser_;
+        private PasswordHasher hasher_;
+        private string error;
+        #endregion
+
+        #region Constructors
+        public UserManagerViewModel(User user, UserRepository repository, PasswordHasher hasher)
+        {
+            this.currentUser_ = user;
+            this.repository_ = repository;
+            this.hasher_ = hasher;
+            error = String.Empty;
+        }
+        #endregion
+
+        #region Properties
+
+        public string Username
+        {
+            get
+            {
+                return currentUser_.Username;
+            }
+            set
+            {
+                currentUser_.Username = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return currentUser_.Password;
+            }
+            set
+            {
+                currentUser_.Password = value;
+            }
+        }
+
+        public string Realname
+        {
+            get
+            {
+                return currentUser_.Realname;
+            }
+            set
+            {
+                currentUser_.Realname = value;
+            }
+        }
+
+        public ObservableCollection<User> Users
+        {
+            get
+            {
+                return repository_.users;
+            }            
+        }
+
+        #endregion
+
+        public void AddUser()
+        {
+            currentUser_.Password = hasher_.GetHash(currentUser_.Password, currentUser_.Username);
+
+            if (repository_.AddUser(currentUser_))
+                currentUser_ = new User(); // clear the current user, so a new one can be inserted
+
+        }
+
+        public bool IsUserValid()
+        {
+            User selectedUser = repository_.GetUser(Username);
+            if (selectedUser.IsNullOrEmpty())
+            {
+                error = "Unknown username";
+                return false;
+            }
+            if (!hasher_.VerifyHash(Password, selectedUser.Password, Username))
+            {
+                error = "Passwords do not match";
+                return false;
+            }
+
+            error = String.Empty;
+            return true;
+        }
+
+        #region IDataErrorInfo
+        string IDataErrorInfo.Error
+        {
+            get { return error; }
+        }
+
+        string IDataErrorInfo.this[string propertyName]
+        {
+            get 
+            {
+                return (currentUser_ as IDataErrorInfo)[propertyName];
+            }
+        }
+        #endregion
+    }
+
+}
---
+++ UserManager/UserManagerTest/UserManagerViewModelTest.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.ComponentModel;
+using NUnit.Framework;
+using UserManager.ViewModel;
+using UserManager.Model.Users;
+using UserManager.Model.Encryption;
+
+namespace UserManagerTest
+{
+    [TestFixture]
+    public class UserManagerViewModelTest
+    {
+        [Test]
+        public void addingNewUserUserHashingFunction()
+        {
+            User user = new User();
+            UserRepository repo = new UserRepository();
+            PasswordHasher hasherAlgorithm = new HmacSha512Hasher();
+            UserManagerViewModel target = new UserManagerViewModel(user, repo, hasherAlgorithm);
+
+            target.Username = "mige";
+            target.Password = "teste";
+            target.Realname = "Mig";
+            target.AddUser();
+
+            Assert.IsTrue(hasherAlgorithm.VerifyHash("teste", target.Users[0].Password, "mige"));
+        }
+
+        [Test]
+        public void verifyThatAddingUserWithErrorsFails()
+        {
+            User user = new User();
+            UserRepository repo = new UserRepository();
+            PasswordHasher hasherAlgorithm = new HmacSha512Hasher();
+            UserManagerViewModel target = new UserManagerViewModel(user, repo, hasherAlgorithm);
+
+            target.Username = "Mmige*";
+            target.Password = "teste";
+            target.Realname = "Mig";
+            string error = (target as IDataErrorInfo)["Username"];
+            Assert.IsFalse(String.IsNullOrEmpty(error), "Should have received error message");
+
+            target.AddUser();            
+            Assert.AreEqual(0, target.Users.Count, "Should have not addded the user");
+        }
+
+        [Test]
+        public void verifyThatAfterAddingUserUIisCleared()
+        {
+            User user = new User();
+            UserRepository repo = new UserRepository();
+            PasswordHasher hasherAlgorithm = new HmacSha512Hasher();
+            UserManagerViewModel target = new UserManagerViewModel(user, repo, hasherAlgorithm);
+
+            target.Username = "mige";
+            target.Password = "teste";
+            target.Realname = "Mig";
+            target.AddUser();
+
+            Assert.IsTrue(String.IsNullOrEmpty(target.Username));
+            Assert.IsTrue(String.IsNullOrEmpty(target.Password));
+            Assert.IsTrue(String.IsNullOrEmpty(target.Realname));
+        }
+
+        [Test]
+        public void verifyThatUserIsValid()
+        {
+            User user = new User();
+            UserRepository repo = new UserRepository();
+            PasswordHasher hasherAlgorithm = new HmacSha512Hasher();
+            UserManagerViewModel target = new UserManagerViewModel(user, repo, hasherAlgorithm);
+
+            target.Username = "mige";
+            target.Password = "teste";
+            target.Realname = "Mig";
+            target.AddUser();
+            Assert.AreEqual(1, target.Users.Count, "Should have addded the user");
+
+            //User is clear
+            Assert.IsTrue(String.IsNullOrEmpty(target.Username));
+            Assert.IsTrue(String.IsNullOrEmpty(target.Password));
+            Assert.IsTrue(String.IsNullOrEmpty(target.Realname));
+
+            //Test User
+            target.Username = "mige";
+            target.Password = "teste";
+            Assert.IsTrue(target.IsUserValid(), "User should be valid");
+            Assert.IsTrue(String.IsNullOrEmpty((target as IDataErrorInfo).Error), "There should not be an error message");
+        }
+
+        [Test]
+        public void verifyUsernameIsInvalid()
+        {
+            User user = new User();
+            UserRepository repo = new UserRepository();
+            PasswordHasher hasherAlgorithm = new HmacSha512Hasher();
+            UserManagerViewModel target = new UserManagerViewModel(user, repo, hasherAlgorithm);
+
+            target.Username = "mige";
+            target.Password = "teste";
+            target.Realname = "Mig";
+            target.AddUser();
+            Assert.AreEqual(1, target.Users.Count, "Should have addded the user");
+
+            //User is clear
+            Assert.IsTrue(String.IsNullOrEmpty(target.Username));
+            Assert.IsTrue(String.IsNullOrEmpty(target.Password));
+            Assert.IsTrue(String.IsNullOrEmpty(target.Realname));
+
+            //Test User
+            target.Username = "migel";
+            target.Password = "teste";
+            Assert.IsFalse(target.IsUserValid(), "User should be invalid");
+            Assert.IsFalse(String.IsNullOrEmpty((target as IDataErrorInfo).Error), "There should be an error message");
+        }
+
+        [Test]
+        public void verifyPasswordIsWrong()
+        {
+            User user = new User();
+            UserRepository repo = new UserRepository();
+            PasswordHasher hasherAlgorithm = new HmacSha512Hasher();
+            UserManagerViewModel target = new UserManagerViewModel(user, repo, hasherAlgorithm);
+
+            target.Username = "mige";
+            target.Password = "teste";
+            target.Realname = "Mig";
+            target.AddUser();
+            Assert.AreEqual(1, target.Users.Count, "Should have addded the user");
+
+            //User is clear
+            Assert.IsTrue(String.IsNullOrEmpty(target.Username));
+            Assert.IsTrue(String.IsNullOrEmpty(target.Password));
+            Assert.IsTrue(String.IsNullOrEmpty(target.Realname));
+
+            //Test User
+            target.Username = "mige";
+            target.Password = "bacalhau";
+            Assert.IsFalse(target.IsUserValid(), "User should be invalid");
+            Assert.AreEqual("Passwords do not match", (target as IDataErrorInfo).Error, "Error messages should match");
+        }
+
+        [Test]
+        public void verifyThatAfterCorrectingPropertiesAndUserValidNoErrorMessagesExist()
+        {
+            User user = new User();
+            UserRepository repo = new UserRepository();
+            PasswordHasher hasherAlgorithm = new HmacSha512Hasher();
+            UserManagerViewModel target = new UserManagerViewModel(user, repo, hasherAlgorithm);
+
+            target.Username = "mige";
+            target.Password = "teste";
+            target.Realname = "Mig";
+            target.AddUser();
+            Assert.AreEqual(1, target.Users.Count, "Should have addded the user");
+
+            //Test User - Fail
+            target.Username = "migel";
+            target.Password = "teste";
+            Assert.IsFalse(target.IsUserValid(), "User should be invalid");
+            Assert.IsFalse(String.IsNullOrEmpty((target as IDataErrorInfo).Error), "There should be an error message");
+
+            //Test User - Pass
+            target.Username = "mige";
+            target.Password = "teste";
+            Assert.IsTrue(target.IsUserValid(), "User should be valid");
+            Assert.IsTrue(String.IsNullOrEmpty((target as IDataErrorInfo).Error), "There should not be an error message");
+        }
+    }
+}
