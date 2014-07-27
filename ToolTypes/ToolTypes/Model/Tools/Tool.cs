7daebea4c4d111bda3778465f35ac24f695bbe4a
mig-l-f <miguelandre.fernandes@gmail.com>
2014-07-27 20:16:47 +0100

    modified location of tools files

---
+++ ToolTypes/ToolTypes/Model/Tools/Tool.cs
@@ -0,0 +1,20 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace ToolTypes.Model.Tools
+{
+    [DataContract]
+    public class Tool
+    {
+        [DataMember(IsRequired=true)]
+        public int toolID { get; set; }
+
+        [DataMember(IsRequired=true)]
+        public String toolLabel { get; set; }
+    }
+}
---
+++ ToolTypes/ToolTypes/Model/Tools/ToolList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace ToolTypes.Model.Tools
+{
+    [DataContract]
+    public class ToolList
+    {
+        public ToolList()
+        {
+            toollist = new List<Tool>();
+        }
+
+        [DataMember]
+        public List<Tool> toollist { get; set; }
+        //public Tool[] toollist { get; set;}
+    }
+}
